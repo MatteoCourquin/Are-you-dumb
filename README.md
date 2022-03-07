692422dfac964b1fe20f2a8e8a62d0626829c796
« Matteo <matteo.courquin@gmail.com>
2022-03-07 11:52:32 +0100

    :lipstick: Modif CSS & Completed ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Are-you-dumb
+
+Inspired by <a href="https://www.instagram.com/3coder/" target="_blank">@3coder</a>
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+const title = document.querySelector('h1')
 const btnNo = document.querySelector('.btn-no')
 const btnYes = document.querySelector('.btn-yes')
 const buttons = document.querySelector('.buttons')
 const message = document.querySelector('.message')
+let positionRight;
+let positionTop;
 
 btnYes.addEventListener('click', () => {
     message.style.display = "block"
     buttons.style.display = "none"
+    title.style.display = "none"
 })
+
 function entierAleatoire(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let positionRight;
-let positionTop;
 function changePosition(positionTop, positionRight) {
-    positionTop = entierAleatoire(0, 90)
+    positionTop = entierAleatoire(1, 90)
     positionRight = entierAleatoire(3, 30)
     btnNo.style.top = `${positionTop}%`
     btnNo.style.right = `${positionRight}%`
